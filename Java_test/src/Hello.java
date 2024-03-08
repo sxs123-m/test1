3275492a1e41f00bf58f84127d9da9d72ff8c846
Admin <13664664+qyxinhua_0@user.noreply.gitee.com>
2024-03-08 19:35:34 +0800

    first commit

--- Java_test/src/Hello.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author 孙显圣
- * @version 1.0
- */
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello, 我是一个测试的文件");
-    }
-}
--- Java_test/src/fdg.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author 孙显圣
- * @version 1.0
- */
-public class fdg {
-}
