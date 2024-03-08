e4085dc9392d5256e07c984291c4d626ac8a54ce
Admin <13664664+qyxinhua_0@user.noreply.gitee.com>
2024-03-08 19:45:15 +0800

    first commit

---
+++ Java_test/src/hi.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author 孙显圣
+ * @version 1.0
+ */
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("daf");
+    }
+}
