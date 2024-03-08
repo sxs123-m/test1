3af386e83d5ae9df6582f1a00b2f0295085cdc46
Admin <13664664+qyxinhua_0@user.noreply.gitee.com>
2024-03-08 20:00:24 +0800

    first commit

---
+++ Java_test/src/hello1.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author 孙显圣
+ * @version 1.0
+ */
+public class hello1 {
+    public static void main(String[] args) {
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("dsfdfsdf dsfsfsdfs/*/*/*/**/*/*/*/");
+        System.out.println("");
+    }
+}
